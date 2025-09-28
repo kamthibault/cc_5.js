735c31459f56d615c9d3f09bb5e6c1f7b26fdd4e
Kameron Thibault <kameronthibault@usf.edu>
2025-09-28 19:00:54 -0400

    Complete

--- cc_5.js
+++ cc_5.js
@@ -4,12 +4,33 @@ let employees = [
     { name: "Josh", Hourlyrate: 17.72, Hoursworked: 22},                          
 ]
 
+let grosspay = employees.Hoursworked*employees.Hourlyrate
 function calculateBasePay (Hourlyrate,Hoursworked){
-let basePay = 0;
-for (let basePay of employees) {
-    basePay = (Hourlyrate*Hoursworked)
+if (employees.Hoursworked < 40)
+    return (grosspay)
+else
+    return ("Overtime")
+
+} 
+
+let overtimePay = employees.hoursworked*employees.Hourlyrate
+function calculateOvertimePay (Hourlyrate,Hoursworked) {
+    if (employees.Hoursworked > 40)
+        return (overtimePay)
 
 }
-console.log (basePay)
+
+function Calculatetaxes (grosspay) {
+    const tax = 0.15
+    return (grosspay*0.15)
+}
+function processPayroll (employees){
+let employees = 3
+
+while (employees > 0) {
+console.log(`${employees.name} x${grosspay*Calculatetaxes}x${overtimePay*Calculatetaxes}@ ${formatCurrency(unit)} = ${formatCurrency(line)}`);
+    sub += line;
+
 }
 
+}
