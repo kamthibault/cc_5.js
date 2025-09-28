623b558ad42dcd35c18ebd4b47850431a40ab14d
Kameron Thibault <kameronthibault@usf.edu>
2025-09-28 18:23:04 -0400

    Step 1

--- cc_5.js
+++ cc_5.js
@@ -1,3 +1,15 @@
-let employees [
-    { name = "john" };
+let employees = [
+    { name: "john", Hourlyrate: 16.28, Hoursworked: 33  },
+    { name: "Tim", Hourlyrate: 18.42, Hoursworked: 45},
+    { name: "Josh", Hourlyrate: 17.72, Hoursworked: 22},                          
 ]
+
+function calculateBasePay (Hourlyrate,Hoursworked){
+let basePay = 0;
+for (let basePay of employees) {
+    basePay = (Hourlyrate*Hoursworked)
+
+}
+console.log (basePay)
+}
+
